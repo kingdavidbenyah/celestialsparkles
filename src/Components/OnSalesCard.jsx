9b1f870e007637ddbc4d15e669196ee167a3924a
King David Adom Benyah <159707020+kingdavidbenyah@users.noreply.github.com>
2025-05-02 05:02:05 +0000

    Update OnSalesCard.jsx

--- src/Components/OnSalesCard.jsx
+++ src/Components/OnSalesCard.jsx
@@ -145,7 +145,7 @@ const OnSalesCard = ({ product, handleAddToCart, isAdded }) => {
         >
           <p className="flex justify-center items-center">
             <span className="text-base md:text-[18px] pr-2 -mt-1">
-              <IoBagCheckOutline />
+              {isAdded ? <IoBagCheckOutline /> : <IoBagAddOutline />}
             </span>
             {isAdded ? "Added to Cart" : "Add to Cart"}
           </p>
